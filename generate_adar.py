5a6c727dc513e085784d73dad3a76078d821aef7
Matt Clarke <matt.clarke@esss.se>
2022-09-14 10:43:56 +0200

    added scripts for generating and plotting adar

---
+++ generate_adar.py
@@ -0,0 +1,37 @@
+import datetime
+import time
+
+import matplotlib.image as mpimg
+from confluent_kafka import Producer
+from matplotlib.cbook import get_sample_data
+from streaming_data_types import serialise_ADAr
+
+OUTPUT_TOPIC = "FREIA_detector"
+BROKER = "localhost:9092"
+
+img = mpimg.imread(get_sample_data("grace_hopper.jpg"))
+img = img[:, :, 0].copy()
+num_pixels = img.shape[0] * img.shape[1]
+
+# Add horizontal guideline
+for x in range(img.shape[1]):
+    img[img.shape[0] // 2 - 1][x] = 0
+    img[img.shape[0] // 2][x] = 0
+    img[img.shape[0] // 2 + 1][x] = 0
+
+# Add vertical guideline
+for x in range(img.shape[0]):
+    img[x][img.shape[1] // 2 - 1] = 0
+    img[x][img.shape[1] // 2] = 0
+    img[x][img.shape[1] // 2 + 1] = 0
+
+config = {"bootstrap.servers": BROKER}
+producer = Producer(**config)
+
+while True:
+    buffer = serialise_ADAr("grace", int(time.time()), datetime.datetime.now(),
+                            img)
+    producer.produce(OUTPUT_TOPIC, buffer)
+    producer.flush()
+    time.sleep(5)
+
---
+++ plot_adar.py
@@ -0,0 +1,38 @@
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+from confluent_kafka import Consumer, TopicPartition
+from streaming_data_types import deserialise_ADAr
+
+TOPIC = "FREIA_detector"
+BROKER = "localhost:9092"
+
+consumer = Consumer(
+    {
+        "bootstrap.servers": BROKER,
+        "group.id": f"consumer-{time.time_ns()}",
+        "auto.offset.reset": "latest",
+    }
+)
+
+metadata = consumer.list_topics(TOPIC)
+if TOPIC not in metadata.topics:
+    raise Exception("Topic does not exist")
+
+topic_partitions = [
+    TopicPartition(TOPIC, p) for p in metadata.topics[TOPIC].partitions
+]
+
+consumer.assign(topic_partitions)
+
+while (msg := consumer.poll(timeout=0.5)) is None:
+    pass
+
+result = deserialise_ADAr(msg.value())
+
+fig = plt.figure(1)
+x, y = np.meshgrid(list(range(result.data.shape[0] + 1)), list(range(result.data.shape[1] + 1)))
+ax = fig.add_subplot(111)
+ax.pcolormesh(x, y, result.data.T)
+plt.show()
