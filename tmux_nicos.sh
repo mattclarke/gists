30bd84f34461d20b0793015e63f7b6a45025364c
Matt Clarke <matt.clarke@ess.eu>
2023-02-22 09:17:40 +0100

    tmux script for nicos

--- tmux_nicos.sh
+++ tmux_nicos.sh
@@ -16,4 +16,4 @@ tmux send-keys ". venv/bin/activate" C-m
 tmux split-window -t $session:1
 tmux send-keys ". venv/bin/activate" C-m
 
-tmux attach-session -t $session
+tmux attach-session -t $session:0
