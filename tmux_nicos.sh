7ae02ab47edc94380a7942136eb8587f0da54ce0
Matt Clarke <matt.clarke@ess.eu>
2023-08-03 11:53:46 +0200

    use .venv rather than venv

--- tmux_nicos.sh
+++ tmux_nicos.sh
@@ -3,17 +3,17 @@ session="nicos"
 
 tmux start-server
 tmux new-session -d -s $session
-tmux send-keys ". venv/bin/activate" C-m
+tmux send-keys ". .venv/bin/activate" C-m
 tmux split-window -ht $session:0
-tmux send-keys ". venv/bin/activate" C-m
+tmux send-keys ". .venv/bin/activate" C-m
 
 tmux new-window -t $session:1 -n services
-tmux send-keys ". venv/bin/activate" C-m
+tmux send-keys ". .venv/bin/activate" C-m
 tmux split-window -t $session:1
-tmux send-keys ". venv/bin/activate" C-m
+tmux send-keys ". .venv/bin/activate" C-m
 tmux split-window -t $session:1
-tmux send-keys ". venv/bin/activate" C-m
+tmux send-keys ". .venv/bin/activate" C-m
 tmux split-window -t $session:1
-tmux send-keys ". venv/bin/activate" C-m
+tmux send-keys ". .venv/bin/activate" C-m
 
 tmux attach-session -t $session:0
